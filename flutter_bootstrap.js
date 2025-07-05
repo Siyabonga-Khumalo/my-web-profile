1009402f195a4437a960d2a28a0f9ff631d0bfb0
Siyabonga-Khumalo <mpreach.smk@gmail.com>
2025-07-05 08:27:48 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4046047707"
+    serviceWorkerVersion: "2855680357"
   }
 });
